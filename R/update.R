c0f6baddcb4cd2d3b9738e25c5b87b128b1f7b37
Gregory Jefferis <jefferis@gmail.com>
2020-02-23 14:30:29 +0000

    selfupdate

--- R/update.R
+++ R/update.R
@@ -1,23 +1,27 @@
 #' Update the `natmanager` package itself.
 #'
-#' @param source Location from which to obtain package updates. Defaults to
-#'   GITHUB since this may well have a newer version than the CRAN packgae
-#'   repository.
+#' @param source Location from which to obtain a newer version of natmanager.
+#'   Defaults to GITHUB since this may well have a newer version than the CRAN
+#'   packgae repository.
 #' @param ... extra arguments to pass to \code{\link[remotes]{install_github}}
 #'   or \code{\link[remotes]{install_cran}}.
+#'   @param dependencies.upgrade See
 #' @export
 #' @seealso \code{\link{install}}
 #' @examples
 #' \dontrun{
-#' library('natmanager')
-#' update()
+#' natmanager::selfupdate()
 #' }
-update <- function(source = c('GITHUB', 'CRAN'), ...) {
+selfupdate <- function(source = c('GITHUB', 'CRAN'),
+                       dependencies.upgrade='never', ...) {
   source <- match.arg(source)
 
-  if (source == 'CRAN') {
-    remotes::install_cran('natmanager', ...)
-  } else {
-    remotes::install_github('natverse/natmanager', ...)
-  }
+  with_envvars({
+    if (source == 'CRAN') {
+      remotes::install_cran('natmanager', ...)
+    } else {
+      remotes::install_github('natverse/natmanager',
+                              upgrade=dependencies.upgrade, ...)
+    }
+  })
 }

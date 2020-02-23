a6fcae0ca930f2be3e955c7d86bd9bceb8b6ee2c
Gregory Jefferis <jefferis@gmail.com>
2020-02-23 15:37:12 +0000

    Add (private) functions to help github updates

--- R/update.R
+++ R/update.R
@@ -25,3 +25,33 @@ selfupdate <- function(source = c('GITHUB', 'CRAN'),
     }
   })
 }
+
+
+github_version <- function(repo='natverse/natmanager') {
+  tryCatch({
+    u=sprintf('https://raw.githubusercontent.com/%s/master/DESCRIPTION', repo)
+    con <- curl::curl(u, open='r')
+    on.exit(close(con))
+    ver.str=read.dcf(con, fields = 'Version')
+    package_version(ver.str)
+  }, error=function(e) NA)
+}
+
+github_newer <- function() {
+  remote=github_version('natverse/natmanager')
+  if(is.na(remote)) return(NA)
+  local=utils::packageVersion('natmanager')
+  remote>local
+}
+
+smartupdate <- function(...) {
+  if (isTRUE(github_newer()) && interactive()) {
+    res = utils::askYesNo(
+      paste0("There is a newer version of natmanager. Would you like to update\n",
+      " (recommended!)"),
+      default = TRUE
+    )
+    if (isTRUE(res))
+      selfupdate(...)
+  }
+}

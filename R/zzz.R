bd77e164404adf0e858e9379a5e71c88f4866523
Gregory Jefferis <jefferis@gmail.com>
2020-02-27 17:18:52 +0000

    Always stop if pat is unset

--- R/zzz.R
+++ R/zzz.R
@@ -5,7 +5,7 @@
   invisible()
 }
 
-#' @importFrom usethis ui_todo ui_code ui_oops ui_yeah
+#' @importFrom usethis ui_todo ui_code ui_oops ui_yeah ui_done ui_stop
 #'
 #' @description \code{check_pat} can be used to check if you have a GITHUB_PAT
 #'   set and will advise on how to do this if necessary.
@@ -36,11 +36,14 @@ check_pat <- function() {
       } else {
         ui_todo("When ready, get your GitHub PAT with {ui_code('usethis::browse_github_token()')}")
       }
+      ui_stop("No GITHUB_PAT set")
     } else {
       stop("Please run natmanager::install() in an interactive R session!\n",
            "See http://natverse.org/install/ for details!")
     }
   } else{
     # nothing to do right now
+    ui_done("GITHUB_PAT is set.")
+    invisible(TRUE)
   }
 }

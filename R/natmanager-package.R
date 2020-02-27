5f2d53d616ec77fee1e8d50a67c1f0c53667f083
Gregory Jefferis <jefferis@gmail.com>
2020-02-27 16:34:14 +0000

    Package docs

---
+++ R/natmanager-package.R
@@ -0,0 +1,18 @@
+#' @seealso \code{\link{install}}
+#' @examples
+#' \dontrun{
+#' # install core packages to try out the core natverse
+#' natmanager::install('core')
+#'
+#' # Full "batteries included" installation with all packages
+#' # You need a GitHub account and personal access token (PAT) for this
+#' natmanager::install('natverse')
+#' }
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL

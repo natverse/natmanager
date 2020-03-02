cbc410d5e7267465f3fd95f7b4abe4475409685d
Sri <j.sridharrajan@gmail.com>
2020-03-02 10:56:23 +0000

    Updated test case for core installation.

--- tests/testthat/test-install.R
+++ tests/testthat/test-install.R
@@ -7,9 +7,12 @@ options(repos = r)
 test_that("installation works", {
 
   #Run tests on travis only if the GITHUB_PAT is set
-  if (!nzchar(Sys.getenv('GITHUB_PAT'))) {
-    skip("Skipping as Github PAT is not set")
-  }
+  #if (!nzchar(Sys.getenv('GITHUB_PAT'))) {
+  #   skip("Skipping as Github PAT is not set")
+  #}
+
+
+
 
   # This test requires installation and hence only run on travis..
   skip_on_cran()
@@ -25,9 +28,16 @@ test_that("installation works", {
     remove.packages(pkgname, lib = liblocs)
   }
 
+  #unset the pat, as the core installation can run without it..
+  old=Sys.getenv("GITHUB_PAT")
+  Sys.setenv("GITHUB_PAT" = '')
+
   natmanager::install(collection = 'core', dependencies = TRUE,
-                      upgrade.dependencies = FALSE, lib = liblocs)
+                      upgrade.dependencies = TRUE, lib = liblocs)
   expect_equal(requireNamespace(pkgname, lib.loc = liblocs, quietly=TRUE),TRUE)
+
+  #Set the pat again..
+  Sys.setenv("GITHUB_PAT" = old)
 
 })
 

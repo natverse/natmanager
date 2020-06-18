2d426d6e45e514fed3ff00ea3870382ce23ffddd
Gregory Jefferis <jefferis@gmail.com>
2020-06-18 14:05:55 +0000

    Private functions for system requirements
    * especially Xquartz on macosx

--- R/utils.R
+++ R/utils.R
@@ -14,3 +14,23 @@ list_repo <- function(orgname = 'natverse') {
   repos <- gh::gh(paste0("/orgs/",orgname,"/repos"), type = "public")
   vapply(repos, "[[", "", "name")
 }
+
+have_xquartz <- function() {
+  isTRUE(nzchar(Sys.which('Xquartz')))
+}
+
+system_requirements_ok <- function() {
+  if(isTRUE(.Platform$OS.type == "windows")) {
+    return(TRUE)
+  }
+  sysname=Sys.info()["sysname"]
+  if(isTRUE(sysname=='Darwin')) {
+    if(!have_xquartz()){
+      usethis::ui_todo(paste("Please download and install Xquartz from",
+                       "https://www.xquartz.org!\nThis is a system requirement",
+                       "and is needed for 3D display of neurons."))
+      return(FALSE)
+    }
+  }
+  return(TRUE)
+}
